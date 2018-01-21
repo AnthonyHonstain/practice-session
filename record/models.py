a5998ab91d86a6d640f4b4cd01f1891f38a1e1e0
Anthony Honstain <honstain@gmail.com>
2018-01-20 19:28:10 -0800

    Add initial model and urls to display a practice session model (my names are not very good for domain)

--- record/models.py
+++ record/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class PracticeSession(models.Model):
+  start = models.DateTimeField()
+  finish = models.DateTimeField()
+  type = models.CharField(max_length=20)
+
+  def __str__(self):
+    return str.format('{0} {1}', self.type, self.start)
--- record/views.py
+++ record/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from record.models import PracticeSession
+
+
+class IndexView(generic.ListView):
+  template_name = 'record/index.html'
+  context_object_name = 'practice_session_list'
+
+  def get_queryset(self):
+    """Return the current sessions"""
+    return PracticeSession.objects.all()
+
