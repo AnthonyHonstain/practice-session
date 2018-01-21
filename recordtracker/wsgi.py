e342506b3e9f125d45e1030837cd027d5a176e3f
Anthony Honstain <honstain@gmail.com>
2018-01-20 18:16:38 -0800

    Moving the settings files around to simplify collect static and integration with heroku (removing the separate directory for recordtracker/settings directory that had common.py/dev.py/etc)

--- recordtracker/wsgi.py
+++ recordtracker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recordtracker.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recordtracker.dev")
 
 application = get_wsgi_application()
