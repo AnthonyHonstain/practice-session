5203c165ddd9dae22b88dfe3563b293ac118c902
Anthony Honstain <honstain@gmail.com>
2018-02-19 19:37:59 -0800

    Adding more fields to the model.

--- record/models.py
+++ record/models.py
@@ -5,6 +5,9 @@ class PracticeSession(models.Model):
   start = models.DateTimeField()
   finish = models.DateTimeField(null=True, blank=True)
   type = models.CharField(max_length=20)
+  rating = models.SmallIntegerField(null=True)
+  feel = models.CharField(max_length=20, null=True)
+  attemptCount = models.IntegerField(null=True)
 
   def __str__(self):
     return str.format('{0} {1}', self.type, self.start)
